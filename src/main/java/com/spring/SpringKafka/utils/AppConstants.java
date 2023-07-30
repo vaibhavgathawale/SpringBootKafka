039d94195001e5e9b543e937cd98cbd578f68690
Hacker <vaibhavgathawale@gmail.com>
2023-07-30 17:40:28 +0530

    Revert "Initial Commit"
    This reverts commit be0e66950694b9a46f5212976753d696787e4ee2.

--- src/main/java/com/spring/SpringKafka/utils/AppConstants.java
+++
@@ -1,6 +0,0 @@
-package com.spring.SpringKafka.utils;
-
-public class AppConstants {
-    public static final String TOPIC_NAME = "Vaibhav";
-    public static final String GROUP_ID = "group_id";
-}
