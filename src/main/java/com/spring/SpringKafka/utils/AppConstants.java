9cc8c5cdeaf0d3f9bb06f69c0ebca3a42eaf6e07
Hacker <vaibhavgathawale@gmail.com>
2023-07-30 19:22:20 +0530

    Initial Commit

---
+++ src/main/java/com/spring/SpringKafka/utils/AppConstants.java
@@ -0,0 +1,6 @@
+package com.spring.SpringKafka.utils;
+
+public class AppConstants {
+    public static final String TOPIC_NAME = "Vaibhav";
+    public static final String GROUP_ID = "group_id";
+}
